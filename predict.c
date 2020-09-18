f53605dade8f6a46f3b40f6a76c5c9d31eb2bf01
Ben Wibking <benwibking@gmail.com>
2020-09-18 14:58:59 +1000

    added compile option FREEZE_ALL_EXCEPT_DM

--- predict.c
+++ predict.c
@@ -116,7 +116,7 @@ void drift_particle(int i, integertime time1)
         dt_drift = (time1 - time0) * All.Timebase_interval;
     
     
-//#if !defined(FREEZE_HYDRO)
+#if !defined(FREEZE_HYDRO) && !defined(FREEZE_ALL_EXCEPT_DM)
 #if defined(HYDRO_MESHLESS_FINITE_VOLUME)
     if(P[i].Type==0) {advect_mesh_point(i,dt_drift);} else {for(j=0;j<3;j++) {P[i].Pos[j] += P[i].Vel[j] * dt_drift;}}
 #elif (SINGLE_STAR_TIMESTEPPING > 0)
@@ -142,7 +142,16 @@ void drift_particle(int i, integertime time1)
 #else
     for(j=0;j<3;j++) {P[i].Pos[j] += P[i].Vel[j] * dt_drift;}
 #endif
-//#endif // FREEZE_HYDRO clause
+#endif // FREEZE_HYDRO ; FREEZE_ALL_EXCEPT_DM
+
+#if defined(FREEZE_ALL_EXCEPT_DM) // we still need to advance DM particles in this case
+    if (P[i].Type==1) { // halo particle type
+      for(j=0;j<3;j++) {
+	P[i].Pos[j] += P[i].Vel[j] * dt_drift;
+      }
+    }
+#endif // FREEZE_ALL_EXCEPT_DM
+    
 #if (NUMDIMS==1)
     P[i].Pos[1]=P[i].Pos[2]=0; // force zero-ing
 #endif
