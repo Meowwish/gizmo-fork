cfa833797219c85d338886b304dcf9c5533f0f19
Ben Wibking <benwibking@gmail.com>
2020-10-17 23:08:50 +1100

    updated slugWrapper

--- galaxy_sf/slug_wrapper.h
+++ galaxy_sf/slug_wrapper.h
@@ -13,7 +13,7 @@ constexpr auto stellar_tracks = "modp020.dat";
 constexpr auto spectral_synthesis = "sb99";
 constexpr auto spectral_filter = "QH0";
 constexpr auto yield_table = "SNII_Sukhbold16_nodecay";
-constexpr auto compute_yields_after_advance = false; // *much* faster!
+constexpr auto recompute_yields_after_reconstruct = false; // *much* faster!
 
 constexpr auto slug_cluster_internal_ID = 1;
 constexpr auto slug_cluster_internal_time = 0.;
@@ -36,7 +36,7 @@ public:
                                 slug_predef.imf(imf_type, minimum_stochastic_mass, stochastic_sampling_type)),
             slug_predef.filter_set(spectral_filter), nullptr, nullptr,
             slug_predef.yields(yield_table), nullptr,
-            slug_predef.ostreams, nullptr, do_stochastic_only, compute_yields_after_advance)
+            slug_predef.ostreams, nullptr, do_stochastic_only)
   {
   }
 
@@ -50,7 +50,7 @@ public:
                                          slug_predef.imf(imf_type, minimum_stochastic_mass, stochastic_sampling_type)),
                      slug_predef.filter_set(spectral_filter), nullptr, nullptr,
                      slug_predef.yields(yield_table), nullptr,
-                     slug_predef.ostreams, nullptr, do_stochastic_only, compute_yields_after_advance)
+                     slug_predef.ostreams, nullptr, do_stochastic_only, recompute_yields_after_reconstruct)
   {
   }
 
