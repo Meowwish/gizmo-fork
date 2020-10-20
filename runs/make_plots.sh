cf438102b719686e8dd9c8ed3286b99256f582b0
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2020-10-20 19:25:34 +1100

    updated analyis script

--- runs/make_plots.sh
+++ runs/make_plots.sh
@@ -4,4 +4,6 @@ module load python3/3.7.4
 module load parallel
 
 MAXPROCS=8
-find output -name '*.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
+OUTPUT_DIR=output_nonmagnetic
+
+find $OUTPUT_DIR -name '*.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
