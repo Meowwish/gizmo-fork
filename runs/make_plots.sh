4a0f2c45b3dc370331166ee31a1bde290408232a
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2020-10-23 10:48:37 +1100

    updated PBS scripts

--- runs/make_plots.sh
+++ runs/make_plots.sh
@@ -4,6 +4,8 @@ module load python3/3.7.4
 module load parallel
 
 MAXPROCS=8
-OUTPUT_DIR=output_nonmagnetic
+OUTPUT_DIR=output
 
+mv plots/*.png .
 find $OUTPUT_DIR -name '*.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
+mv *.png plots/
