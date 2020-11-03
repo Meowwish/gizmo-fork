8ccfe8ecc1c3f3dcc542238a2b3913db998a35df
Ben Wibking <benwibking@gmail.com>
2020-11-04 09:29:25 +1100

    updated plotting

--- runs/make_plots.sh
+++ runs/make_plots.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-module load python3/3.7.4
-module load parallel
-
 MAXPROCS=8
 OUTPUT_DIR=output
 
-mv plots/*.png .
 find $OUTPUT_DIR -name '*.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
-mv *.png plots/
+
