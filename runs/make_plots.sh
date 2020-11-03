60d6d4fa0470ca4ccf6061783ea8607efb4d4730
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2020-11-03 19:59:58 +1100

    update Makefile.include for Gadi

--- runs/make_plots.sh
+++ runs/make_plots.sh
@@ -6,6 +6,5 @@ module load parallel
 MAXPROCS=8
 OUTPUT_DIR=output
 
-mv plots/*.png .
 find $OUTPUT_DIR -name '*.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
-mv *.png plots/
+
