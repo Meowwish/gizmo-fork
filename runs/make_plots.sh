d7651c1189e458800d650608bd2a3d1f91f0457a
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2020-11-30 14:08:17 +1100

    update plotting script

--- runs/make_plots.sh
+++ runs/make_plots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAXPROCS=4
-OUTPUT_DIR=output_lowres
+OUTPUT_DIR=output_agora_hires
 
 find $OUTPUT_DIR -name '*.0.hdf5' | parallel --bar --max-procs $MAXPROCS python3 plot_meshless.py 
 
